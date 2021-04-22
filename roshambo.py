99f1f05954d3a7af516ebff78d45f2d641e25413
Adam Essmyer <acemyer@Adams-MBP.lan1>
2021-04-21 20:58:08 -0500

    finished code block, tested game

--- roshambo.py
+++ roshambo.py
@@ -1,21 +1,23 @@
 import random
+
 user_action = input("Enter a choice (ro, sham, bo): ")
 possible_actions = ["ro", "sham", "bo"]
 computer_action = random.choice(possible_actions)
 
 if user_action == computer_action:
     print(f"Both players selected {user_action}. It's a tie!")
-    elif user_action == "Ro":
+elif user_action == "Ro":
+    if computer_action == "Bo":
         print("Ro smashes Bo! You win!")
-        else:
+    else:
             print("Sham covers Ro! You lose.")
-    elif user_action == "sham":
-        if computer_action == "ro":
+elif user_action == "sham":
+    if computer_action == "ro":
             print("sham covers ro! You win!")
-        else:
+    else:
             print("Bo cuts sham! You lose.")
-    elif user_action == "Bo"
-        if computer_action == "sham":
+elif user_action == "Bo":
+    if computer_action == "sham":
         print("Bo cuts sham! You win!")
-        else:
+    else:
             print("Ro smashes Bo! You lose.")
