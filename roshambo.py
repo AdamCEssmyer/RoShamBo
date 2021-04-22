8a74feac767e19f231aa9dd126df594c85863dbd
Adam Essmyer <acemyer@Adams-MBP.lan1>
2021-04-21 19:27:33 -0500

    created possible action function

--- roshambo.py
+++ roshambo.py
@@ -1,2 +1,4 @@
 import random
-user_action = input("Enter a choice (ro, sham, bo"): ")
+user_action = input("Enter a choice (ro, sham, bo): ")
+possible_actions = ["ro", "sham", "bo"]
+computer_action = random.choice(possible_actions)
